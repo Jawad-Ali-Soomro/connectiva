c8ece3f1641364dddffbe7699ff112994728c266
Jawad-Ali-Soomro <jawadal829@gmail.com>
2025-08-15 11:59:47 +0500

    feat/favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,6 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" type="image/x-icon" href="/logo.png" />
       <body className={inter.className}>
         <Navbar />
         {/* AOS initializer */}
